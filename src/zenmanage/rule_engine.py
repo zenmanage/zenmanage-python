039e03571e5464fbf26818fb874ec83b5443c772
zenmanage-brk <ben.kubs@zenmanage.com>
2026-05-31 15:15:45 -0500

    Additional code quality updates.

--- src/zenmanage/rule_engine.py
+++ src/zenmanage/rule_engine.py
@@ -50,7 +50,9 @@ class RuleEngine:
 
         attribute = context.get_attribute(attribute_name)
         if attribute is None:
-            negated_ops = {"notequal", "notin", "notcontains", "notstartswith", "notendswith", "isnull"}
+            negated_ops = {
+                "notequal", "notin", "notcontains", "notstartswith", "notendswith", "isnull"
+            }
             if operator in negated_ops:
                 return True
             return False
